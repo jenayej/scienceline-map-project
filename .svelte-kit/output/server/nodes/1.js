ae2cc9178ddf97b05d37cf7aaf9ee6ad1730d52c
Sara Chodosh <sara.chodosh@nytimes.com>
2024-09-10 20:33:01 -0400

    try again

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CXk5ol8d.js","_app/immutable/chunks/scheduler.D2CNYxju.js","_app/immutable/chunks/index.Bfd7QkMT.js","_app/immutable/chunks/entry.D9BwwLFG.js"];
-export const stylesheets = [];
-export const fonts = [];
--- node_modules/@reuters-graphics/graphics-svelte-components/Framer/stores.js
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const width = writable(660);
