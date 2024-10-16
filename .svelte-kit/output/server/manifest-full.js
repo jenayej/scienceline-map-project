14d5f78aa3ebe13cff31a551650eec9167482429
Sara Chodosh <sara.chodosh@nytimes.com>
2024-10-16 15:08:47 -0400

    try build

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.png"]),
+	assets: new Set(["favicon.png","illos_vf/beaver.png","illos_vf/bunker.png","illos_vf/eye.png","illos_vf/insulation.png","illos_vf/mammoth.png","illos_vf/megaphone.png","illos_vf/newtown.png","illos_vf/soilbank.png","illos_vf/winery.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.BODqHfGj.js","app":"_app/immutable/entry/app.DFMHenqh.js","imports":["_app/immutable/entry/start.BODqHfGj.js","_app/immutable/chunks/entry.CdDJTAYF.js","_app/immutable/chunks/scheduler.D2CNYxju.js","_app/immutable/entry/app.DFMHenqh.js","_app/immutable/chunks/scheduler.D2CNYxju.js","_app/immutable/chunks/index.Bfd7QkMT.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.CdP0XV4d.js","app":"_app/immutable/entry/app.CsnE6gph.js","imports":["_app/immutable/entry/start.CdP0XV4d.js","_app/immutable/chunks/entry.CySY6oqq.js","_app/immutable/chunks/scheduler.D2CNYxju.js","_app/immutable/entry/app.CsnE6gph.js","_app/immutable/chunks/scheduler.D2CNYxju.js","_app/immutable/chunks/index.Bfd7QkMT.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
